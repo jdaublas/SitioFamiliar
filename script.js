635390a84b4c21c2172731806bb8d02f4600e987
jdaublas <87877758+jdaublas@users.noreply.github.com>
2021-08-29 15:08:32 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-200";
+}
+
+let navLinks = document.getElementById("navLinks");
+let showMenu = onclick;
+let hideMenu = onclick;
